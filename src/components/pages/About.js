76192c1e07a722061fa5f30984bcd79d900ad4ba
Mayur192004 <mayurdeshmukh464@gmail.com>
2024-08-21 23:03:54 +0530

    Tentative Commit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,9 @@ export default function About({isLoggedin} ) {
   return (
     <div>
       <Navbar isLoggedin={isLoggedin} ></Navbar>
+      {/* Will be doing it in future Scope */}
       Future Implementation
+    
     </div>
   )
 }
